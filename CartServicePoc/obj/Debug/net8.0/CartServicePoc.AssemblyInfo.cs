93e17dc41405b75291019c4b49e98e4beecee7c4
Filip Jurković <filipjurko13@gmail.com>
2026-02-27 15:09:37 +0100

    dockerizing basic POC

--- CartServicePoc/obj/Debug/net8.0/CartServicePoc.AssemblyInfo.cs
+++ CartServicePoc/obj/Debug/net8.0/CartServicePoc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CartServicePoc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f951b32a43ee22efa98f527f0c474bddfcb90be")]
 [assembly: System.Reflection.AssemblyProductAttribute("CartServicePoc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CartServicePoc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
