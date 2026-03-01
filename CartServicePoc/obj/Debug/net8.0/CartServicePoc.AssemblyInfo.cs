4b7e26050579255f53757b91654dc01a3916e6ef
Filip Jurković <filipjurko13@gmail.com>
2026-03-01 15:19:47 +0100

    adding our pdf doc and adding health checks to docker compose

--- CartServicePoc/obj/Debug/net8.0/CartServicePoc.AssemblyInfo.cs
+++ CartServicePoc/obj/Debug/net8.0/CartServicePoc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CartServicePoc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a015de3d81f7d659c8065b95cacbbb106e8d4ddb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CartServicePoc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CartServicePoc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
